399bbfb967f6d670ff7a869b281db950cc681d9d
mehrdadmoradabadi <mehrdadmoradabadi@yahoo.com>
2025-01-29 12:51:06 +0200

    Fixed Info classes

--- src/main/java/com/wallboard/wallboard/resource/adapters/TrunkInfo.java
+++ src/main/java/com/wallboard/wallboard/resource/adapters/TrunkInfo.java
@@ -1,11 +1,10 @@
 package com.wallboard.wallboard.resource.adapters;
 
-import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
